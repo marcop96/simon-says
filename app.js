43743630bf771eafeda3faceefc890696b657aac
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-05 17:30:04 +0200

    create function that sets up the game
    hides elements not necessary after the game starts

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const $PlayButton = document.querySelector(".btn-start");
+let gameStarted = new Boolean(false);
+$PlayButton.onclick = function () {
+  prepareStartGame();
+};
 
+function prepareStartGame() {
+  const $startText = document.querySelector(".start-text");
+  gameStarted = true;
+  if (gameStarted == true) {
+    $PlayButton.classList.add("hidden");
+    $startText.classList.add("hidden");
+  }
+}
+
+console.log(gameStarted);
