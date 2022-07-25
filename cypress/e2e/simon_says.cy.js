48c44fbb99568315fa786d8c6f3ea8d00ce8a414
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-07-25 18:22:40 +0200

    test:checks correct load of website

--- cypress/e2e/simon_says.cy.js
+++ cypress/e2e/simon_says.cy.js
@@ -3,21 +3,14 @@
 describe("website", () => {
   it("succesfully loads", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
+    cy.get("[data-cy=play-text]").should("contain.text", "press Play to start");
+    cy.get("[data-cy=play-button]").should("be.visible");
+    cy.get("[data-cy=round-text]").should("not.be.visible");
+    cy.get("[data-cy=turn-text]").should("not.be.visible");
+    cy.get("[data-cy=squares-container]").should("be.visible");
+    cy.get("[data-cy=blue-square]").should("be.visible");
+    cy.get("[data-cy=red-square]").should("be.visible");
+    cy.get("[data-cy=green-square]").should("be.visible");
+    cy.get("[data-cy=yellow-square]").should("be.visible");
   });
 });
-
-describe("checks that there-s a play button", () => {
-  it("passes", () => {
-    cy.get(".btn-start").click();
-  });
-});
-
-describe("round counter", () => {
-  it("round counter works");
-});
-
-// describe("plays wrong move", () => {
-//   it("passes", () => {
-//     cy.get(".btn-start").click();
-//   });
-// });
